af7abde0e59ff2d34fc262aae4c1cc83118ef8a7
Daan Rademaker <daanrademaker@royalfloraholland.com>
2022-03-29 17:01:33 +0200

    add None type to union_pipe test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,15 +7,15 @@ from pydantic_factories.utils import is_union
 
 
 def test_is_union():
-    class UnionPipe(BaseModel):
-        union_pipe: int | str  # Pipe syntax supported from Python 3.10 onwards
+    class UnionTest(BaseModel):
+        union_pipe: int | str | None  # Pipe syntax supported from Python 3.10 onwards
         union_normal: Union[int, str]
         no_union: Any
 
-    class UnionPipeFactory(ModelFactory):
-        __model__ = UnionPipe
+    class UnionTestFactory(ModelFactory):
+        __model__ = UnionTest
 
-    for field_name, model_field in UnionPipeFactory.get_model_fields(UnionPipeFactory._get_model()):
+    for field_name, model_field in UnionTestFactory.get_model_fields(UnionTestFactory._get_model()):
         if (field_name == "union_pipe") or (field_name == "union_normal"):
             assert is_union(model_field)
         else:
