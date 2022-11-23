ed4c02305e74d203ea2d3495ab8719fc7e798914
Avihai <ayosef@paypal.com>
2022-11-23 19:09:38 +0200

    add typedict and dataclass test cases

--- tests/test_factory_auto_registration.py
+++ tests/test_factory_auto_registration.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass as vanilla_dataclass
 from typing import List
 
 from pydantic import BaseModel
+from typing_extensions import TypedDict
 
 from pydantic_factories import ModelFactory
 
@@ -50,3 +52,48 @@ def test_auto_register_model_factory_using_create_factory() -> None:
     assert c.b.b_text == const_value
     assert c.b_list[0].b_text == const_value
     assert c.b.a.a_text != const_value
+
+
+def test_dataclass_model_factory_auto_registration() -> None:
+    @vanilla_dataclass
+    class DataClass:
+        text: str
+
+    class UpperModel(BaseModel):
+        nested_field: DataClass
+        nested_list_field: List[DataClass]
+
+    class UpperModelFactory(ModelFactory):
+        __model__ = UpperModel
+
+    class DataClassFactory(ModelFactory):
+        text = "const value"
+        __model__ = DataClass
+        __auto_register__ = True
+
+    upper = UpperModelFactory.build()
+
+    assert upper.nested_field.text == DataClassFactory.text
+    assert upper.nested_list_field[0].text == DataClassFactory.text
+
+
+def test_typeddict_model_factory_auto_registration() -> None:
+    class TypedDictModel(TypedDict):
+        text: str
+
+    class UpperSchema(BaseModel):
+        nested_field: TypedDictModel
+        nested_list_field: List[TypedDictModel]
+
+    class UpperModelFactory(ModelFactory):
+        __model__ = UpperSchema
+
+    class TypedDictFactory(ModelFactory):
+        text = "const value"
+        __model__ = TypedDictModel
+        __auto_register__ = True
+
+    upper = UpperModelFactory.build()
+
+    assert upper.nested_field["text"] == TypedDictFactory.text
+    assert upper.nested_list_field[0]["text"] == TypedDictFactory.text
