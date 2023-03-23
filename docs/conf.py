a4cba7a826b0ae1efc2f13a6668434eac7cfa13f
Jacob Coffee <jacob@z7x.org>
2023-03-22 20:50:24 -0500

    Apply suggestions from code review
    Co-authored-by: Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -23,38 +23,28 @@ suppress_warnings = [
     "ref.python",  # TODO: remove when https://github.com/sphinx-doc/sphinx/issues/4961 is fixed
 ]
 
-html_theme = "pydata_sphinx_theme"
+html_theme = "starlite_sphinx_theme"
 html_static_path = ["_static"]
 html_css_files = ["style.css"]
-html_js_files = ["versioning.js"]
-html_favicon = "images/favicon.ico"
-html_logo = "images/logo.svg"
 html_show_sourcelink = False
-html_sidebars = {"about/*": []}
 html_title = "Starlite Project | Polyfactory"
 
 html_additional_pages = {"index": "landing.html"}
 
 html_theme_options = {
+    "github_repo_name": "polyfactory",
     "use_edit_page_button": False,
-    "show_toc_level": 4,
-    "navbar_align": "left",
-    "icon_links": [
-        {
-            "name": "GitHub",
-            "url": "https://github.com/starlite-api/polyfactory",
-            "icon": "fa-brands fa-github",
-            "type": "fontawesome",
+    "extra_navbar_items": {
+        "Documentation": "reference/index",
+        "Community": {
+            "Contribution guide": "community/contribution-guide/index",
+            "Code of Conduct": "https://github.com/starlite-api/.github/blob/main/CODE_OF_CONDUCT.md",
         },
-        {
-            "name": "Discord",
-            "url": "https://discord.gg/MmcwxztmQb",
-            "icon": "fa-brands fa-discord",
-            "type": "fontawesome",
+        "About": {
+            "Organization": "about/organization",
+            "Releases": "about/releases",
         },
-    ],
-    "navbar_end": ["navbar-icon-links"],
-    "navbar_persistent": ["search-button", "theme-switcher"],
+    }
 }
 
 html_context = {
