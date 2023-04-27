f90d60e43435696ad0e17359b287e1863d66b0cf
George Sakkis <george.sakkis@gmail.com>
2023-04-28 00:01:46 +0300

    Bson field support for odmantic models (#193)
    * Replace _create_pydantic_type_map with overriding ModelFactory.get_provider_map
    
    * Support BSON types for OdmanticModelFactory
    
    * Update test_odmantic_factory with BSON fields on EmbeddedModel (currently failing)
    
    * BaseFactory._base_factories should contain only classes that explicitly set __is_base_factory__ to True
    
    Also:
    - _get_or_create_factory: Iterate over BaseFactory._base_factories in reverse order (most specific classes first)
    - create_factory: don't inject the attrs of cls to the new dynamically generated class
    
    * Revert "Replace _create_pydantic_type_map with overriding ModelFactory.get_provider_map"
    
    This reverts commit 26bbd55f0d95040dd7d9241828440cd046c4bfb9.

--- polyfactory/factories/odmantic_odm_factory.py
+++ polyfactory/factories/odmantic_odm_factory.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Any, Generic, TypeVar, Union
+
+import decimal
+from typing import TYPE_CHECKING, Any, Callable, Generic, TypeVar, Union
 
 from polyfactory.exceptions import MissingDependencyException
 from polyfactory.factories.pydantic_factory import ModelFactory
 from polyfactory.fields import Ignore
 from polyfactory.utils.predicates import is_safe_subclass
+from polyfactory.value_generators.primitives import create_random_bytes
 
 try:
-    from odmantic import EmbeddedModel, Model
+    from bson.decimal128 import Decimal128, create_decimal128_context
+    from odmantic import EmbeddedModel, Model, bson as odbson
 
 except ImportError as e:
     raise MissingDependencyException("odmantic is not installed") from e
@@ -33,3 +37,25 @@ class OdmanticModelFactory(Generic[T], ModelFactory[T]):
         return is_safe_subclass(value, Model) or is_safe_subclass(value, EmbeddedModel)
 
     id = Ignore()
+
+    @classmethod
+    def get_provider_map(cls) -> dict[Any, Callable[[], Any]]:
+        provider_map = super().get_provider_map()
+        provider_map.update(
+            {
+                odbson.Int64: lambda: odbson.Int64.validate(cls.__faker__.pyint()),
+                odbson.Decimal128: lambda: _to_decimal128(cls.__faker__.pydecimal()),
+                odbson.Binary: lambda: odbson.Binary.validate(create_random_bytes(cls.__random__)),
+                odbson._datetime: lambda: odbson._datetime.validate(cls.__faker__.date_time_between()),
+                # bson.Regex and bson._Pattern not supported as there is no way to generate
+                # a random regular expression with Faker
+                # bson.Regex:
+                # bson._Pattern:
+            }
+        )
+        return provider_map
+
+
+def _to_decimal128(value: decimal.Decimal) -> Decimal128:
+    with decimal.localcontext(create_decimal128_context()) as ctx:
+        return Decimal128(ctx.create_decimal(value))
--- tests/test_odmantic_factory.py
+++ tests/test_odmantic_factory.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from typing import Any, List
 from uuid import UUID
 
+import bson
 import pytest
 from odmantic import AIOEngine, EmbeddedModel, Model
 
@@ -10,15 +12,30 @@ from polyfactory.factories.odmantic_odm_factory import OdmanticModelFactory
 class OtherEmbeddedDocument(EmbeddedModel):
     name: str
     serial: UUID
+    created_on: datetime
+    bson_id: bson.ObjectId
+    bson_int64: bson.Int64
+    bson_dec128: bson.Decimal128
+    bson_binary: bson.Binary
 
 
 class MyEmbeddedDocument(EmbeddedModel):
     name: str
     serial: UUID
     other_embedded_document: OtherEmbeddedDocument
+    created_on: datetime
+    bson_id: bson.ObjectId
+    bson_int64: bson.Int64
+    bson_dec128: bson.Decimal128
+    bson_binary: bson.Binary
 
 
 class MyModel(Model):
+    created_on: datetime
+    bson_id: bson.ObjectId
+    bson_int64: bson.Int64
+    bson_dec128: bson.Decimal128
+    bson_binary: bson.Binary
     name: str
     embedded: MyEmbeddedDocument
     embedded_list: List[MyEmbeddedDocument]
@@ -35,6 +52,30 @@ def test_handles_odmantic_models() -> None:
 
     result = MyFactory.build()
 
-    assert result.name
-    assert result.embedded
-    assert result.embedded_list
+    assert isinstance(result.created_on, datetime)
+    assert isinstance(result.bson_id, bson.ObjectId)
+    assert isinstance(result.bson_int64, bson.Int64)
+    assert isinstance(result.bson_dec128, bson.Decimal128)
+    assert isinstance(result.bson_binary, bson.Binary)
+    assert isinstance(result.name, str)
+    assert isinstance(result.embedded, MyEmbeddedDocument)
+    assert isinstance(result.embedded_list, list)
+    for item in result.embedded_list:
+        assert isinstance(item, MyEmbeddedDocument)
+        assert isinstance(item.name, str)
+        assert isinstance(item.serial, UUID)
+        assert isinstance(item.created_on, datetime)
+        assert isinstance(item.bson_id, bson.ObjectId)
+        assert isinstance(item.bson_int64, bson.Int64)
+        assert isinstance(item.bson_dec128, bson.Decimal128)
+        assert isinstance(item.bson_binary, bson.Binary)
+
+        other = item.other_embedded_document
+        assert isinstance(other, OtherEmbeddedDocument)
+        assert isinstance(other.name, str)
+        assert isinstance(other.serial, UUID)
+        assert isinstance(other.created_on, datetime)
+        assert isinstance(other.bson_id, bson.ObjectId)
+        assert isinstance(other.bson_int64, bson.Int64)
+        assert isinstance(other.bson_dec128, bson.Decimal128)
+        assert isinstance(other.bson_binary, bson.Binary)
