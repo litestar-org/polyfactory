3fd7b782dff04763e5cbfe2d2af9f96d57949cd7
George Sakkis <george.sakkis@gmail.com>
2023-04-30 01:39:12 +0300

    Refactor complex_types

--- polyfactory/value_generators/complex_types.py
+++ polyfactory/value_generators/complex_types.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
-from collections import deque
-from typing import TYPE_CHECKING, Any, MutableMapping, Tuple
+
+from typing import TYPE_CHECKING, AbstractSet, Any, MutableMapping, MutableSequence, Set
 
 from typing_extensions import is_typeddict
 
-from polyfactory.constants import TYPE_MAPPING
 from polyfactory.field_meta import FieldMeta
 from polyfactory.utils.helpers import unwrap_annotation, unwrap_args
 from polyfactory.utils.predicates import get_type_origin, is_any, is_union
@@ -14,11 +13,7 @@ if TYPE_CHECKING:
     from polyfactory.factories.base import BaseFactory
 
 
-def handle_container_type(
-    container_type: type,
-    factory: type[BaseFactory],
-    field_meta: FieldMeta,
-) -> Any:
+def handle_collection_type(field_meta: FieldMeta, container_type: type, factory: type[BaseFactory]) -> Any:
     """Handle generation of container types recursively.
 
     :param container_type: A type that can accept type arguments.
@@ -27,68 +22,54 @@ def handle_container_type(
 
     :returns: A built result.
     """
-    if mapped_container_type := TYPE_MAPPING.get(container_type):
-        container_type = mapped_container_type
+    container = container_type()
+    if not field_meta.children:
+        return container
 
-    container = container_type() if container_type is not frozenset else set()
-
-    if isinstance(container, MutableMapping) or is_typeddict(container):
+    if issubclass(container_type, MutableMapping) or is_typeddict(container_type):
         key_type, value_type = unwrap_args(field_meta.annotation) or (str, str)
-        key = handle_complex_type(field_meta=FieldMeta.from_type(annotation=key_type), factory=factory)
-
+        key = handle_complex_type(FieldMeta.from_type(key_type), factory)
         if is_union(value_type) and field_meta.children:
             value_field_meta = factory.__random__.choice(field_meta.children)
-            value = handle_complex_type(field_meta=value_field_meta, factory=factory)
+            value = handle_complex_type(value_field_meta, factory)
         else:
-            value = handle_complex_type(field_meta=FieldMeta.from_type(annotation=value_type), factory=factory)
+            value = handle_complex_type(FieldMeta.from_type(value_type), factory)
+        container[key] = value
 
-        container[key] = value  # pyright: ignore
-    elif field_meta.children:
-        if isinstance(container, (list, deque)):
-            container.append(
-                handle_complex_type(
-                    field_meta=factory.__random__.choice(field_meta.children),
-                    factory=factory,
-                )
-            )
-        else:
-            container.add(
-                handle_complex_type(
-                    field_meta=factory.__random__.choice(field_meta.children),
-                    factory=factory,
-                )
-            )
+    elif issubclass(container_type, MutableSequence):
+        container.append(handle_complex_type(field_meta.children[0], factory))
 
-    return container if container_type is not frozenset else container_type(*container)
+    elif issubclass(container_type, Set):
+        container.add(handle_complex_type(field_meta.children[0], factory))
+
+    elif issubclass(container_type, AbstractSet):
+        container = container.union(handle_collection_type(field_meta, set, factory))
+
+    elif issubclass(container_type, tuple):
+        container = container_type(handle_complex_type(subfield_meta, factory) for subfield_meta in field_meta.children)
+
+    else:
+        raise NotImplementedError(f"Unsupported container type: {container_type}")
+
+    return container
 
 
-def handle_complex_type(
-    factory: type[BaseFactory],
-    field_meta: FieldMeta,
-) -> Any:
-    """Recursive type generation based on typing info stored in the graph like structure of pydantic field_metas.
+def handle_complex_type(field_meta: FieldMeta, factory: type[BaseFactory]) -> Any:
+    """Recursive type generation based on typing info stored in the graph like structure
+    of pydantic field_metas.
 
-    :param factory: A factory.
     :param field_meta: A field meta instance.
+    :param factory: A factory.
 
     :returns: A built result.
     """
-
-    if origin := get_type_origin(annotation=unwrap_annotation(field_meta.annotation)):
-        if origin not in (tuple, Tuple):
-            return handle_container_type(field_meta=field_meta, container_type=origin, factory=factory)
-
-        return tuple(
-            handle_complex_type(field_meta=sub_field, factory=factory) for sub_field in (field_meta.children or [])
-        )
+    if origin := get_type_origin(unwrap_annotation(field_meta.annotation)):
+        return handle_collection_type(field_meta, origin, factory)
 
     if is_union(field_meta.annotation) and field_meta.children:
-        return handle_complex_type(field_meta=factory.__random__.choice(field_meta.children), factory=factory)
+        return handle_complex_type(factory.__random__.choice(field_meta.children), factory)
 
     if is_any(field_meta.annotation):
-        return create_random_string(random=factory.__random__, min_length=1, max_length=10)
+        return create_random_string(factory.__random__, min_length=1, max_length=10)
 
-    if factory.should_set_none_value(field_meta):
-        return None
-
-    return factory.get_field_value(field_meta=field_meta, field_build_parameters=None)
+    return factory.get_field_value(field_meta)
