61f1e2e0d857ffd111ab8c8d7cfbab6f67230ae1
guacs <126393040+guacs@users.noreply.github.com>
2023-08-02 22:32:06 +0530

    fix: random seed configuration (#321)
    Co-authored-by: Jacob Coffee <jacob@z7x.org>

--- docs/examples/configuration/test_example_2.py
+++ docs/examples/configuration/test_example_2.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from random import Random
 
-from polyfactory import Use
 from polyfactory.factories import DataclassFactory
 
 
@@ -17,11 +16,13 @@ class PersonFactory(DataclassFactory[Person]):
     __model__ = Person
     __random__ = Random(10)
 
-    name = Use(DataclassFactory.__random__.choice, ["John", "Alice", "George"])
+    @classmethod
+    def name(cls) -> str:
+        return cls.__random__.choice(["John", "Alice", "George"])
 
 
 def test_setting_random() -> None:
     # the outcome of 'factory.__random__.choice' is deterministic, because Random is configured with a set value.
-    assert PersonFactory.build().name == "Alice"
+    assert PersonFactory.build().name == "George"
     assert PersonFactory.build().name == "John"
     assert PersonFactory.build().name == "Alice"
