0add277437e27955c465a5029c37ca86531a3e00
Jonathan Liuti <jonathan.liuti@cluepoints.com>
2023-10-18 11:16:57 +0200

    Adapt documentation test

--- docs/examples/configuration/test_example_8.py
+++ docs/examples/configuration/test_example_8.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 from uuid import UUID
 
-from polyfactory import PostGenerated
+import pytest
+
+from polyfactory import ConfigurationException, PostGenerated
 from polyfactory.factories.dataclass_factory import DataclassFactory
 
 
@@ -10,15 +12,12 @@ class Person:
     id: UUID
 
 
-class PersonFactory(DataclassFactory[Person]):
-    __model__ = Person
-    __check_model__ = True
-    unknown_field = PostGenerated(lambda: "foo")
+with pytest.raises(
+    ConfigurationException,
+    match="unknown_field is declared on the factory PersonFactory but it is not part of the model Person",
+):
 
-
-def test_optional_type_ignored() -> None:
-    PersonFactory()
-    """
-    The above will raise:
-    polyfactory.exceptions.ConfigurationException: unknown_field is declared on the factory PersonFactory but it is not part of the model Person
-    """
+    class PersonFactory(DataclassFactory[Person]):
+        __model__ = Person
+        __check_model__ = True
+        unknown_field = PostGenerated(lambda: "foo")
