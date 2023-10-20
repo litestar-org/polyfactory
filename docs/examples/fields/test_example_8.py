11f234c030d1d2546ffbeb062ed736753bc7417c
Andrew Truong <40660973+adhtruong@users.noreply.github.com>
2023-10-20 13:23:18 +0100

    docs: reorder docs and add docs on factories as fields (#418)

---
+++ docs/examples/fields/test_example_8.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+from datetime import date, datetime
+from enum import Enum
+from typing import Any, Dict, List, Union
+from uuid import UUID
+
+from polyfactory.factories import DataclassFactory
+
+
+class Species(str, Enum):
+    CAT = "Cat"
+    DOG = "Dog"
+
+
+@dataclass
+class Pet:
+    name: str
+    species: Species
+    sound: str
+
+
+@dataclass
+class Person:
+    id: UUID
+    name: str
+    hobbies: List[str]
+    age: Union[float, int]
+    birthday: Union[datetime, date]
+    pet: Pet
+    assets: List[Dict[str, Dict[str, Any]]]
+
+
+class PetFactory(DataclassFactory[Pet]):
+    __model__ = Pet
+
+    name = lambda: DataclassFactory.__random__.choice(["Ralph", "Roxy"])
+
+
+class PersonFactory(DataclassFactory[Person]):
+    __model__ = Person
+
+    pet = PetFactory
+
+
+def test_subfactory() -> None:
+    person_instance = PersonFactory.build()
+
+    assert isinstance(person_instance.pet, Pet)
+    assert person_instance.pet.name in ["Ralph", "Roxy"]
+
+    person_instance_with_pet_name = PersonFactory.build(pet={"name": "Winston"})
+    assert person_instance_with_pet_name.pet.name == "Winston"
