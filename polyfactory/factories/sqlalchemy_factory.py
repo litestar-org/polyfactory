d61a78d85f91472414dc2fccd5a0d792104f3655
jeffry <36665036+wangxin688@users.noreply.github.com>
2024-05-09 10:38:35 +0000

    fix(sqla_factory): fix JSON type fake data generation

--- polyfactory/factories/sqlalchemy_factory.py
+++ polyfactory/factories/sqlalchemy_factory.py
@@ -100,7 +100,8 @@ class SQLAlchemyFactory(Generic[T], BaseFactory[T]):
             postgresql.TSTZRANGE: lambda: (cls.__faker__.past_datetime(), datetime.now()),  # noqa: DTZ005
             postgresql.HSTORE: lambda: cls.__faker__.pydict(),
             # `types.JSON` is compatible for sqlachemy extend dialects. Such as `pg.JSON` and `JSONB`
-            types.JSON: lambda: cls.__faker__.pydict(),
+            # accept `str,int,float` as basic types in case `sqlalchemy/sql/sqltypes.json.json_serializer` raise error
+            types.JSON: lambda: cls.__faker__.pydict(value_types=[int, str, float]),
         }
 
     @classmethod
