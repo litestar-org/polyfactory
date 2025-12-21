cc9e3ce192a0e070b38de773fbc546217cbcc26e
Andrew Truong <adh.truong@gmail.com>
2025-12-21 10:40:17 +0000

    fix: add test

--- tests/utils/test_predicates.py
+++ tests/utils/test_predicates.py
@@ -10,6 +10,7 @@ from polyfactory.utils.predicates import is_literal
     [
         (Literal[1, 2, 3], True),
         (Union[int, str], False),
+        (Union[Literal[1], Literal[2]], False),  # noqa: PYI030
         (int, False),
     ],
 )
